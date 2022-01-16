25f87753fbbda5f32005e25de923f029b3e27bfb
lgancarczyk <lukgan2000@gmail.com>
2022-01-16 14:03:41 +0100

    Dodaj pliki projektów.

---
+++ gamblingSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace gamblingSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
